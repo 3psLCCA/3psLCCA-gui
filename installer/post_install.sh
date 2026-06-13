07ee5eaa292554a167b12e6ceb999cd9e242688b
zehen-249 <1hasanmehendi123@gmail.com>
2026-06-13 22:01:37 +0530

    update packaging

--- installer/post_install.sh
+++ installer/post_install.sh
@@ -7,9 +7,11 @@ INSTALL_DIR="$PREFIX"
 
 launch_shortcut="$HOME/.local/share/applications/$APP_NAME.desktop"
 uninstall_shortcut="$HOME/.local/share/applications/Uninstall-$APP_NAME.desktop"
-ICON_FILE="$INSTALL_DIR/logo-3psLCCA.ico"
+ICON_FILE="$INSTALL_DIR/logo-3psLCCA.png"
 LAUNCHER="$INSTALL_DIR/bin/$APP_NAME"
 UNINSTALLER="$INSTALL_DIR/Uninstall-lcca-gui.sh"
+[ -f "$LAUNCHER" ] || echo "Launcher not found: $LAUNCHER"
+[ -f "$UNINSTALLER" ] || echo "Uninstaller not found: $UNINSTALLER"
 
 echo "Creating application launcher..."
 
@@ -21,7 +23,7 @@ Name=$APP_NAME
 Exec=$LAUNCHER
 Icon=$ICON_FILE
 Type=Application
-Categories=Engineering;
+Categories=Science;Education;
 Terminal=false
 EOF
 
@@ -31,19 +33,21 @@ echo "Launcher created at:"
 echo "$launch_shortcut"
 
 echo "Creating application Uninstaller..."
+if [ -f "$UNINSTALLER" ]; then
+    chmod +x "$UNINSTALLER"
 
-cat <<EOF > "$uninstall_shortcut"
+    cat <<EOF > "$uninstall_shortcut"
 [Desktop Entry]
-Name=Uninstall $PP_NAME
+Name=Uninstall $APP_NAME
 Exec=$UNINSTALLER
 Icon=$ICON_FILE
 Type=Application
-Categories=Engineering;
+Categories=Science;Education;
 Terminal=true
 EOF
 
-chmod +x "$UNINSTALLER"
-chmod +x "$uninstall_shortcut"
+    chmod +x "$uninstall_shortcut"
+fi
 
 update-desktop-database ~/.local/share/applications 2>/dev/null || true
 echo "Uninstaller created at:"
